942c4a764295793791f6434a05c9e294b1b45904
KaziSagar <kazi.sagar@northsouth.edu>
2022-11-17 07:52:55 +0600

    initial commit - first build

---
+++ src/test/java/utils/Utils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+    public void takeScreenShot(AndroidDriver driver) throws IOException {
+        File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        String time = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss-aa").format(new Date());
+        String fileWithPath = "./src/test/resources/screenshots/" + time + ".png";
+        FileUtils.copyFile(screenshotFile, new File(fileWithPath));
+    }
+}
